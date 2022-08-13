73a6d4634b111a3a46132cc753375e1b26e48d55
Henrique Greghi <henriquevsg@gmail.com>
2022-08-13 20:19:51 -0300

    criando calculo do tempo de jogo

---
+++ Jogo/Calculo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Jogo
+{
+    class Calculo
+    {
+        public int HoraInicio;
+        public int MinutoInicio;
+        public int HoraFim;
+        public int MinutoFim;
+        public int TotalHora;
+        public int TotalMinuto;
+        public int Hora;
+
+
+        public int HoraJogo()
+        {
+            Hora = (HoraFim * 60) - (HoraInicio * 60) + (MinutoFim - MinutoInicio);
+            TotalHora = Hora / 60;
+
+            return TotalHora;
+        }
+        public int MinutoJogo()
+        {
+            TotalMinuto = TotalHora % 60;
+            return TotalMinuto;
+        }
+        public override string ToString()
+        {
+            
+            if (HoraJogo() == 0 && MinutoJogo() == 0)
+            {
+                return "O jogo durou 24 Horas e 0 Minutos";
+            }
+            else
+            {
+                
+                return ("O jogo durou " + HoraJogo() + " Hora(s) E " + MinutoJogo() +  " Minuto(s) ") ;
+            }
+           
+        }
+    }
+
+}
---
+++ Jogo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Jogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo h = new Calculo();
+
+
+            Console.WriteLine("Entre com hora inicio: ");
+            h.HoraInicio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com minuto inicio: ");
+            h.MinutoInicio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com hora final: ");
+            h.HoraFim = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com minuto final: ");
+            h.MinutoFim = int.Parse(Console.ReadLine());
+            Console.WriteLine(h);
+        }
+    }
+}
